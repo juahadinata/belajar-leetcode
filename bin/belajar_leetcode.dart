e39b518df741f92203d47a0415fc7cb454ab8c2b
Juah Adinata <juahadinata6@gmail.com>
2025-04-13 18:43:51 +0700

    commit pertama

---
+++ bin/belajar_leetcode.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_leetcode/belajar_leetcode.dart' as belajar_leetcode;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_leetcode.calculate()}!');
+}
---
+++ test/belajar_leetcode_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_leetcode/belajar_leetcode.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
